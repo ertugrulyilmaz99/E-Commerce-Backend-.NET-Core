9543fb38c66c3738e7466436f1ad8df5ba0ae022
ERTUĞRUL YILMAZ <ertugrul.yilmaz.bal@gmail.com>
2023-01-13 12:55:45 +0300

    Autofac and validation service with fluentvalidation is added.

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -28,8 +28,10 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddSingleton<IProductService, ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
+            ////It direct the IProductService to ProductManager
+            //services.AddSingleton<IProductService, ProductManager>();
+            ////It direct the IProductDal to EfPRoductDal
+            //services.AddSingleton<IProductDal, EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
