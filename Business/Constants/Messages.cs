ee9dada4ac2a2d4178d2c86295d3270fe19ce1fb
ERTUĞRUL YILMAZ <ertugrul.yilmaz.bal@gmail.com>
2023-01-19 15:23:25 +0300

    Token structure is implemented.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +16,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "There will be at most 10 different products in a category.";
         public static string ProductNameAlreadyExists = "Product already exists.";
         public static string CategoryLimitExceded = "Can't add new product. Category Limit is Exceded.";
+        public static string AuthorizationDenied = "You are not authorized.";
+        public static string UserRegistered = "User is registered.";
+        public static string UserNotFound = "User not found.";
+        public static string PasswordError = "Password error.";
+        public static string SuccessfulLogin = "Login successful.";
+        public static string AccessTokenCreated = "Access token is created.";
+        public static string UserAlreadyExists = "User already exists.";
     }
 }
