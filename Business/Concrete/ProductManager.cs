06fe84361ee32c0276817a4938fe5b6a4241356e
ERTUĞRUL YILMAZ <ertugrul.yilmaz.bal@gmail.com>
2023-01-20 12:20:06 +0300

    Caching is added. Performance metric measurement is learned.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -3,6 +3,7 @@ using Business.BusinessAspects.Autofac;
 using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Business;
@@ -32,6 +33,7 @@ namespace Business.Concrete
 
         [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
         {
 
@@ -51,6 +53,7 @@ namespace Business.Concrete
 
         }
 
+        [CacheAspect]//key, value
         public IDataResult<List<Product>> GetAll()
         {
             if (DateTime.Now.Hour == 15)
@@ -66,6 +69,7 @@ namespace Business.Concrete
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id));
         }
 
+        [CacheAspect]
         public IDataResult<Product> GetById(int productId)
         {
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
@@ -82,6 +86,7 @@ namespace Business.Concrete
         }
 
         [ValidationAspect(typeof(Product))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Update(Product product)
         {
             throw new NotImplementedException();
@@ -117,6 +122,11 @@ namespace Business.Concrete
             }
 
             return new SuccessResult();
+        }
+
+        public IResult AddTransactionalTest(Product product)
+        {
+            throw new NotImplementedException();
         }
     }
 }
